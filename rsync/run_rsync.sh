93dd4489cb422affbaa9bb7f50c6e787dad937fa
Brandon Smith <blsmit5728@gmail.com>
2014-09-11 08:00:03 -0400

    Added The Bridge, was not coming in correctly from Trakt. Also added new disk rsync until I switch over to that disk in the disk1 position.

--- rsync/run_rsync.sh
+++ rsync/run_rsync.sh
@@ -4,4 +4,5 @@ DATE=`date +%m_%d_%Y`
 LOG_PATH="/home/bsmith/.flexget/rsync/rsync_logs/"
 
 rsync -arv --ignore-existing --stats /mnt/disk1/Library/ /mnt/disk2/Library/ >> $LOG_PATH/rsync_log_$DATE.log
+rsync -arv --ignore-existing --stats /mnt/disk1/Library/ /mnt/disk3/Library/ >> $LOG_PATH/disk3_rsync_log_$DATE.log
 
