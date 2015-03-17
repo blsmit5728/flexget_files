62e5d4823c690374f66d132fcbd83c0266bc92d7
Brandon Smith <blsmit5728@gmail.com>
2015-03-17 16:01:44 -0400

    Updated filler

--- plugins/filler.py
+++ plugins/filler.py
@@ -1,3 +1,4 @@
+
 """Filler plugin that does nothing."""
 from __future__ import unicode_literals, division, absolute_import
 import re
@@ -24,10 +25,9 @@ class Filler(object):
     def on_task_output(self, task, config):
         pass
 
-    def search(self, entry, config=None):
+    def search(self, task, entry, config):
         pass
 
 @event('plugin.register')
 def register_plugin():
     plugin.register(Filler, 'filler', groups=['search'], api_ver=2)
-
