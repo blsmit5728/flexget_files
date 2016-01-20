cb4b72e455e29ec44813b445c34b9945ce499573
Brandon Smith <blsmit5728@gmail.com>
2016-01-20 16:41:41 -0500

    upping

--- bin/torrent_complete.py
+++ bin/torrent_complete.py
@@ -44,7 +44,6 @@ if len(sys.argv) != 4:
     log.error('%s called with %d arguments, it requires 3.' % (sys.argv[0],(len(sys.argv)-1)))
     str_called = ""
     for x in range(0,len(sys.argv)):
-        #log.error('%s' % (sys.argv[x]))
         str_called = str_called + str(sys.argv[x]) + " "
     log.error('args: %s' % str_called)
     sys.exit(-1)
@@ -56,6 +55,7 @@ torrent_path=sys.argv[3]
 log.debug("%s called with torrent_id='%s', torrent_name='%s', torrent_path='%s'." % (sys.argv[0],
     torrent_id, torrent_name, torrent_path))
 
+log.debug("RECALL: '%s %s %s %s'" % (sys.argv[0],torrent_id, torrent_name, torrent_path))
 if DOWNLOAD_PATH not in torrent_path:
     log.debug("Torrent '%s' path (%s) not in %s, skipping unrar" % (torrent_name,torrent_path,DOWNLOAD_PATH))
 
@@ -82,6 +82,7 @@ for path, task in FLEXGET_PATH_TASK.items():
             total_size = int(subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, shell=True).communicate()[0].split()[0])
             if main_file_size > (total_size * 0.9):
                 new_name = os.path.join(os.path.dirname(main_file), torrent_name+'.'+main_file.split('.')[-1])
+                log.debug('Names: %s,%s,%s' % (main_file, torrent_name, new_name));
                 log.debug('Renaming %s to %s because it is >90%% of the unpacked torrent'%(os.path.basename(main_file),  os.path.basename(new_name)))
                 os.rename(main_file, new_name)
             else:
