437defc5ae11cf859b9a866f4be1dc321d314b22
Brandon Smith <blsmit5728@gmail.com>
2017-04-07 15:01:18 -0400

    Updated configs

--- bin/tor_comp.sh
+++ bin/tor_comp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -n "/home/bsmith/repos/flexget_files/bin/torrent_complete.py $1 $2 ${3}/ && " >> /home/bsmith/logs/tor_comp/deluge_call.log
+echo -e "/home/bsmith/repos/flexget_files/bin/torrent_complete.py $1 $2 ${3}/ \n" >> /home/bsmith/logs/tor_comp/deluge_call.log
 python /home/bsmith/repos/flexget_files/bin/torrent_complete.py $1 $2 ${3} 
 #D=`date +%m-%d-%y_%H:%M:%S`
 #echo -n "/home/bsmith/repos/flexget_files/bin/torrent_complete.py $1 $2 ${3}/ && " >> /home/bsmith/logs/tor_comp/deluge_call.log
